a83b56f5dd685a8ee38d4500b181548c80ba98b7
XceligentITDept <itdept@xceligent.com>
2016-01-15 11:10:25 -0600

    updates

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /
-mkdir ~/scripts
-export PATH="$PATH:~/scripts"
-cd ~/scripts
+mkdir /scripts
+export PATH="$PATH:/scripts"
+cd /scripts
 apt-get update
 apt-get install git
 wget --no-check-certificate https://github.com/XceligentITDept/DesktopCentral/raw/master/dc.sh
