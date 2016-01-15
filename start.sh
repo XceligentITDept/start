30dd624c6aa62c764fbd573923fb71105a886569
XceligentITDept <itdept@xceligent.com>
2016-01-15 11:03:51 -0600

    fixing script
    Trying again

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+cd /
 mkdir ~/scripts
 export PATH="$PATH:~/scripts"
 cd ~/scripts
 apt-get update
 apt-get install git
 wget --no-check-certificate https://github.com/XceligentITDept/DesktopCentral/raw/master/dc.sh
+chmod +x cd.sh
 ./dc.sh
