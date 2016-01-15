c3ba682cc2ee0ee246948a501202845d6c493aa2
XceligentITDept <itdept@xceligent.com>
2016-01-15 10:56:19 -0600

    changes

--- start.sh
+++ start.sh
@@ -5,3 +5,5 @@ export PATH="$PATH:~/scripts"
 cd ~/scripts
 apt-get update
 apt-get install git
+wget --no-check-certificate https://github.com/XceligentITDept/DesktopCentral/raw/master/dc.sh
+./dc.sh
