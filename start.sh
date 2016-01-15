01195a2780d526d5788f3504a844c3895ac84765
XceligentITDept <itdept@xceligent.com>
2016-01-15 11:15:02 -0600

    again

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ cd /scripts
 apt-get update
 apt-get install git
 wget --no-check-certificate https://github.com/XceligentITDept/DesktopCentral/raw/master/dc.sh
-chmod +x /scripts/cd.sh
+chmod +x /scripts/dc.sh
 /scripts/dc.sh
