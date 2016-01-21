b30a761a2cede78209db2c29efacec28aea47788
XceligentITDept <itdept@xceligent.com>
2016-01-21 13:54:30 -0600

    added salt.sh
    Added for systems that already ran start.sh

---
+++ salt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# for adding salt-minion
+
+# add line to /etc/apt/sources.list
+sed -i "\$a## Added for Salt-Minion Install - kbennett 1/20/16" /etc/apt/sources.list
+sed -i "\$adeb http://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest trusty main" /etc/apt/sources.list
+
+# then apt-get update
+sudo apt-get update
+
+# then install 
+sudo apt-get install salt-minion
